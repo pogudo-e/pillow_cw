c4373c167c2fe2ee83c204314b73ae50511b29e0
Pogudo Evgeny <sensesbit@gmail.com>
2024-01-26 00:39:48 +0300

    - Добавил favicon.ico (как заглушка) - Временно все вычисления в классе CodeWars - Убрал ошибки при отрисовке изображения - Переместил путь к получению картинки на уровень глубже (теперь /name/{user_name})

--- CodeWars.py
+++ CodeWars.py
@@ -1,5 +1,7 @@
-from parser import parse_json
-from DrawImage import DrawImage
+import json
+import urllib.request
+
+from PIL import Image, ImageDraw, ImageFont
 
 
 class CodeWars:
@@ -12,13 +14,36 @@ class CodeWars:
         self.user_name = user_name
 
     def parse(self):
-        (self.honor,
-         self.leader_pos,
-         self.overall,
-         self.total_completed) = parse_json(self.user_name)
+        dict_j = None
+        try:
+            url = f'https://www.codewars.com/api/v1/users/{self.user_name}'
+            with urllib.request.urlopen(url) as response:
+                body_json = response.read()
+            dict_j = json.loads(body_json)
+            if "success" in dict_j and dict_j["success"] == 0:
+                raise ValueError("Request failed")
+        except ValueError:
+            print('Не загрузилось :(')
+        self.honor = dict_j['honor']
+        self.leader_pos = dict_j['leaderboardPosition']
+        self.overall = dict_j['ranks']['overall']['name']
+        self.total_completed = dict_j['codeChallenges']['totalCompleted']
 
     def draw(self):
-        self.img = DrawImage(self.user_name, self.honor, self.overall, self.leader_pos, self.total_completed)
+        self.img = Image.open('src/img/pcw.png')
+
+        i_draw = ImageDraw.Draw(self.img)
+        handle = ImageFont.truetype('src/fonts/ubuntu_medium.ttf', size=32)
+        i_draw.text((225, 12), text=str(self.user_name), font=handle, fill='#8C8C8C')  # имя пользователя
+        handle_sub = ImageFont.truetype('src/fonts/ubuntu_medium.ttf', size=16)  # load font for sub text
+        i_draw.text((113, 59), text=f'{self.honor}', font=handle_sub, fill='#D2D5D7')  # honor
+        i_draw.text((227, 82), text=str(self.leader_pos), font=handle_sub, fill='#D2D5D7')  # leaderboard position
+        i_draw.text((122, 106), text=str(self.overall), font=handle_sub, fill='#D2D5D7')  # overall
+        i_draw.text((231, 130), text=str(self.total_completed), font=handle_sub, fill='#D2D5D7')  # total completed kata
+        self.img.save('./src/output/canvas.png')
+
+    def show(self):
+        self.img.show()
 
     def draw_view(self):
         if self.img:
@@ -32,3 +57,11 @@ class CodeWars:
                 f'{self.overall=}\n'
                 f'{self.leader_pos=}\n'
                 f'{self.total_completed=}\n')
+
+    def __del__(self):
+        self.img = None
+        self.total_completed = None
+        self.leader_pos = None
+        self.overall = None
+        self.honor = None
+        self.user_name = None
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
+import os
+import time
+
 from CodeWars import CodeWars
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 # TODO: исправить ошибки рисования
 
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/{user_name}")
+
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = "favicon.ico"
+    file_path = os.path.join("static", file_name)
+    return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
+
+
+@app.get("/name/{user_name}")
 def root(user_name: str):
+    time.sleep(2)
     test = CodeWars(f'{user_name}')
     test.parse()
     print(test)
     test.draw()
+    del test
     return FileResponse('./src/output/canvas.png')
